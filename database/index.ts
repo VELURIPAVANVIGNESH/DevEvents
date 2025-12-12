f8547fdf04d1d03fe67d85ac104bc3f95bea040d
VELURIPAVANVIGNESH <157129397+VELURIPAVANVIGNESH@users.noreply.github.com>
2025-12-13 01:35:46 +0530

    Revert "implement database models"

--- database/index.ts
+++
@@ -1,4 +0,0 @@
-export { Event } from "./event.model";
-export { Booking } from "./booking.model";
-export type { EventFields, EventDocument, EventMode } from "./event.model";
-export type { BookingFields, BookingDocument } from "./booking.model";
